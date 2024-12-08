1e1c0bfa34d54ca8967969f1729d01dd7295a68b
PPRB <loic@beebs.app>
2024-12-08 22:05:41 +0100

    improve core config

---
+++ core/core1/src/index.js
@@ -0,0 +1,9 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const pack2_1 = __importDefault(require("@monorepo-ts/pack2"));
+console.log("This is core");
+(0, pack2_1.default)('core');
+//# sourceMappingURL=index.js.map
---
+++ core/core2/src/index.js
@@ -0,0 +1,9 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const pack1_1 = __importDefault(require("@monorepo-ts/pack1"));
+console.log("This is core 2");
+(0, pack1_1.default)('core 2');
+//# sourceMappingURL=index.js.map
